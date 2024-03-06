28f1b1ced3b51a2dff3ff460580fa47878b0aac2
mat25 <mateojean25660@gmail.com>
2024-03-06 17:50:06 +0100

    responsive

--- Portfolio-final/index.php
+++ Portfolio-final/index.php
@@ -117,7 +117,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </header>
     <div id="fullpage">
-        <div class="section" >
+        <div class="section placeNav" >
             <div class="accueil">
                 <h1>Matéo JEAN <br> Développeur Back-End</h1>
                 <a href="#presentation"class="fleche-accueil" id="flecheScroll">
@@ -126,7 +126,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
         <!--    Page présentation    -->
-        <div class="section"  >
+        <div class="section placeNav"  >
             <div id="carouselExampleIndicators" class="carousel slide presentation-large">
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -190,12 +190,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="page-passion w-75 d-flex justify-content-center align-item-center flex-column mx-auto">
                     <h1 class="text-center">Passions</h1>
                     <div class="row">
-                        <div class="col col-6">
+                        <div class="col col-12 col-md-6 mt-md-0 mt-3">
                             <h3 class="text-center">Sports</h3>
                             <img class="w-100" src="images/ski.png" alt="Image ski">
                             <img class="w-100" src="images/vtt.jpg" alt="Image Vélo">
                         </div>
-                        <div class=" col col-6">
+                        <div class=" col col-12 col-md-6 mt-md-0 mt-3">
                             <h3 class="text-center">Sports Mécanique</h3>
                             <img class="w-100" src="images/FabioQuartararo.jpg" alt="Image Moto">
                             <img class="w-100" src="images/WRC-fourmaux.jpg" alt="Image WRC">
@@ -206,12 +206,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         </div>
         <!--    Page CV    -->
-        <div class="section">
+        <div class="section placeNav">
             <h1 class="text-center">CV</h1>
             <div class="page-cv mx-5">
                 <div class="row justify-content-center align-items-center">
-                    <div class="col-3 contenu-col-gauche pe-0">
-                        <img class="w-50 rounded " src="images/photoCV.jpg" alt="photo de profil">
+                    <div class="col col-12 col-md-6 col-xl-3 contenu-col-gauche pe-0">
+                        <div class="imageCvDiv">
+                            <img class="rounded imageCV" src="images/photoCV.jpg" alt="photo de profil">
+                        </div>
                         <div class="coordonnees">
                             <p class="mt-4"><a href="https://maps.app.goo.gl/WhDDvcKQpuLvWBXNA" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
@@ -232,7 +234,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             </svg> Télécharger mon CV</a></p>
                         </div>
                     </div>
-                    <div class="col-4 ps-0">
+                    <div class="col col-12 col-md-6 col-xl-4 ps-0">
                         <div class="competenceProfessionnel">
                             <h3>Compétences</h3>
                             <div class="competence">
@@ -252,7 +254,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             </ul>
                         </div>
                     </div>
-                    <div class="col-5">
+                    <div class="col col-12 col-xl-5">
                         <h3>Expériences professionnelles</h3>
                         <div class="accordion accordion-flush" id="accordionCompetenceInfo">
                             <div class="accordion-item">
@@ -357,8 +359,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
             </div>
         </div>
-        <div class="section" >
-            <div class="project w-75 mx-auto">
+        <div class="section placeNav" >
+            <div class="project-test w-75 mx-auto">
                 <h1 class="text-center">Projets</h1>
                 <div class="row">
                     <div class="col">
@@ -401,24 +403,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             </div>
         </div>
-        <div class="section" >
-            <h1>Veille technologique</h1>
-            <p>En construction </p>
+        <div class="section placeNav" >
+            <div class="veilleTechno">
+                <h1>Veille technologique</h1>
+                <p>En construction </p>
+            </div>
         </div>
-        <div class="section">
+        <div class="section placeNav">
             <div class="contact">
                 <h1 class="text-center mb-4">Contactez moi</h1>
                 <div class="row">
-                    <form class="col col-8" method="post">
+                    <form class="col col-12 col-xl-8" method="post">
                         <div class="row">
-                            <div class="col">
+                            <div class="col col-12 col-xl-6">
                                 <label for="inputNom" class="text d-block">Nom</label>
                                 <input type="text" class="w-100 mb-4 px-1" id="inputNom" name="nom">
                                 <?php if (!empty($erreurs["nom"])) { ?>
                                     <p>Erreur</p>
                                 <?php } ?>
                             </div>
-                            <div class="col">
+                            <div class="col col-12 col-xl-6">
                                 <label for="inputPrenom" class="block">Prénom</label>
                                 <input type="text" class="w-100 mb-4 px-1" id="inputPrenom" name="prenom">
                             </div>
@@ -436,35 +440,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <button type="submit" class="btn btn-secondary mt-3 w-25" id="envoyer">Envoyer</button>
                         </div>
                     </form>
-                    <div class="col col-4 coordonnee">
-                        <a href="tel:0769745610">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5"/>
-                            </svg>
-                            <br>
-                            <p>07 69 74 56 10</p>
-                        </a>
-                        <a href="mailto:mateojean25660@gmail.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
-                                <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671"/>
-                                <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791"/>
-                            </svg>
-                            <br>
-                            <p>mateojean25660@gmail.com</p>
-                        </a>
-                        <a href="https://github.com/mat25" target="_blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-                            </svg>
-                            <p>Github</p>
-                        </a>
-                        <a href="https://maps.app.goo.gl/WhDDvcKQpuLvWBXNA" target="_blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-                                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
-                            </svg>
-                            <br>
-                            <p>Montrond le chateau</p>
-                        </a>
+                    <div class="col col-12 col-xl-4 coordonnee my-5 my-xl-0">
+                        <div class="row w-100">
+                            <div class="col col-12 col-sm-6 col-xl-12">
+                                <a href="tel:0769745610">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5"/>
+                                    </svg>
+                                    <br>
+                                    <p>07 69 74 56 10</p>
+                                </a>
+                            </div>
+                            <div class="col col-12 col-sm-6 col-xl-12">
+                                <a href="mailto:mateojean25660@gmail.com">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
+                                        <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671"/>
+                                        <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791"/>
+                                    </svg>
+                                    <br>
+                                    <p>mateojean25660@gmail.com</p>
+                                </a>
+                            </div>
+                            <div class="col col-12 col-sm-6 col-xl-12">
+                                <a href="https://github.com/mat25" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                                    </svg>
+                                    <p>Github</p>
+                                </a>
+                            </div>
+                            <div class="col col-12 col-sm-6 col-xl-12">
+                                <a href="https://maps.app.goo.gl/WhDDvcKQpuLvWBXNA" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                                        <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+                                    </svg>
+                                    <br>
+                                    <p>Montrond le chateau</p>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -530,6 +544,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     var contactElement = document.querySelector('[data-anchor="contact"]');
                     fullpage_api.destroy('all');
                     isFullPageInitialized = false;
+                    // Ajout des ID correspondants
                     accueilElement.id = "accueil";
                     presentationElement.id = "presentation";
                     cvElement.id = "CV";
@@ -584,6 +599,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 showActiveTooltip:false,
                 slidesNavigation:true,
             })
+        }
+
+        if (window.innerWidth < 1200) {
+            var accueil = document.querySelector(".accueil").parentElement;
+            accueil.id="accueil";
+
+            var presentation = document.querySelector(".presentation-small").parentElement;
+            presentation.id = "presentation";
+
+            var cv = document.querySelector(".page-cv").parentElement;
+            cv.id = "CV";
+
+            var project = document.querySelector(".project-test").parentElement;
+            project.id = "projet";
+
+            var vt = document.querySelector(".veilleTechno").parentElement;
+            vt.id = "veille-techno";
+
+            var contact = document.querySelector(".contact").parentElement;
+            contact.id = "contact";
         }
     </script>
 
