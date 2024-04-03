29f43f6c1a0a3eb5b4125817b02fb98db95c77fa
mat25 <mateojean25660@gmail.com>
2024-04-03 13:45:34 +0200

    Ajout certif

--- Portfolio-final/index.php
+++ Portfolio-final/index.php
@@ -273,9 +273,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 <li>WINDEV</li>
                             </ul>
                             <h3>Certifications</h3>
-                            <a href="">CNIL</a>
-                            <a href="">ANSSI</a>
-                            <a href="">PIX</a>
+                            <ul>
+                                <li>
+                                    <a href="pdf/Attestation-pix-Matéo-JEAN.pdf" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                                    </svg> PIX </a>
+                                </li>
+                                <li>
+                                    <a href="pdf/Attestation-CNIL-Matéo-Jean.pdf" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                                    </svg> CNIL </a>
+                                </li>
+                                <li>
+                                    <a href="pdf/Attestation-anssi-Matéo-Jean.pdf" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+                                        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+                                    </svg> ANSSI </a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                     <div class="col col-12 col-xl-5">
